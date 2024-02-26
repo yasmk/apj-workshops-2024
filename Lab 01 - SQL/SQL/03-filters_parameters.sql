3557b586fe42295edfbff2d2bbe8a6b9d3e1e2fc
AbhiDatabricks <AbhiDatabricks>
2024-02-26 03:41:06 +0000

    Fix params

--- Lab 01 - SQL/SQL/03-filters_parameters.sql
+++ Lab 01 - SQL/SQL/03-filters_parameters.sql
@@ -7,43 +7,27 @@ USE <<catalog>>.<<databasename>>;;
 --------------------
 
 -- FOR FILTERING
-SELECT
-  a.store_id,
-  a.order_source,
-  a.order_state,
-  b.city,
-  b.country_code,
-  b.name AS store_name,
-  count(*) AS cnt
-FROM
-  fact_apj_sales a
-  JOIN dim_store_locations b ON a.slocation_skey = b.slocation_skey
-GROUP BY
-  a.store_id,
-  a.order_source,
-  a.order_state,
-  b.city,
-  b.country_code,
-  b.name;
+select
+  locations.city,
+  count(*) as cnt
+from
+ fact_apj_sales sales
+  join fact_apj_sale_items items 
+       on items.sale_id = sales.sale_id
+  join dim_locations locations
+       on sales.store_id = locations.id
+group by locations.city;
 
 
 -- FOR PARAMETERS
-SELECT
-  a.store_id,
-  a.order_source,
-  a.order_state,
-  b.city,
-  b.country_code,
-  b.name AS store_name,
-  count(*) AS cnt
-FROM
-  fact_apj_sales a
-  JOIN dim_store_locations b ON a.slocation_skey = b.slocation_skey
-GROUP BY
-  a.store_id,
-  a.order_source,
-  a.order_state,
-  b.city,
-  b.country_code,
-  b.name
-HAVING cnt > 5 -- replace this with a parameter
+select
+  locations.city,
+  count(*) as cnt
+from
+ fact_apj_sales sales
+  join fact_apj_sale_items items 
+       on items.sale_id = sales.sale_id
+  join dim_locations locations
+       on sales.store_id = locations.id
+group by locations.city
+HAVING cnt > 100000 -- replace this with a parameter
