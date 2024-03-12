dafcb7d6a74d8c30dab3ee6b3b20b75245067e73
mlevit <mlevit>
2024-03-12 05:45:07 +0000

    Code cleanup

--- Lab 01 - SQL/SQL/02-view.sql
+++ Lab 01 - SQL/SQL/02-view.sql
@@ -4,7 +4,7 @@ USE catalog.database;
 
 -- Select all records from the fact table and join it
 -- with the 'items' and 'locations' dimensions
-select
+SELECT
   sales.ts::timestamp as date,
   sales.store_id,
   sales.sale_id,
@@ -12,27 +12,26 @@ select
   locations.city
 from
  fact_apj_sales sales
-  join fact_apj_sale_items items 
-       on items.sale_id = sales.sale_id
-  join dim_locations locations
-       on sales.store_id = locations.id;
-
+  JOIN fact_apj_sale_items items 
+       ON items.sale_id = sales.sale_id
+  JOIN dim_locations locations
+       ON sales.store_id = locations.id;
 
 
 -- Create a reusable view of the above Select statement
 CREATE VIEW IF NOT EXISTS vw_sales_cost_location AS
-select
+SELECT
   sales.ts::timestamp as date,
   sales.store_id,
   sales.sale_id,
   items.product_cost::double as cost,
   locations.city
-from
+FROM
  fact_apj_sales sales
-  join fact_apj_sale_items items 
-       on items.sale_id = sales.sale_id
-  join dim_locations locations
-       on sales.store_id = locations.id;
+  JOIN fact_apj_sale_items items 
+       ON items.sale_id = sales.sale_id
+  JOIN dim_locations locations
+       ON sales.store_id = locations.id;
   
 
 -- Select all reocrds from the view
