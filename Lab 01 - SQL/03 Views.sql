ed0a93d330330e67df766368318910677d330f60
Marat Levit <mlevit@gmail.com>
2024-06-20 19:33:02 +1000

    Added MV

--- Lab 01 - SQL/03 Views.sql
+++ Lab 01 - SQL/03 Views.sql
@@ -32,3 +32,31 @@ FROM
 
 -- Select all reocrds from the view
 SELECT * FROM catalog.database.vw_sales_cost_location;
+
+
+-- Describe the view
+DESCRIBE EXTENDED catalog.database.vw_sales_cost_location;
+
+
+-- Create a materialized view of the above Select statement
+CREATE MATERIALIZED VIEW IF NOT EXISTS catalog.database.mv_sales_cost_location AS
+SELECT
+  sales.ts::timestamp as date,
+  sales.store_id,
+  sales.sale_id,
+  items.product_cost::double as cost,
+  locations.city
+FROM
+ catalog.database.fact_apj_sales sales
+  JOIN catalog.database.fact_apj_sale_items items 
+       ON items.sale_id = sales.sale_id
+  JOIN catalog.database.dim_locations locations
+       ON sales.store_id = locations.id;
+  
+
+-- Select all reocrds from the view
+SELECT * FROM catalog.database.mv_sales_cost_location;
+
+
+-- Describe the materialized view
+DESCRIBE EXTENDED catalog.database.mv_sales_cost_location;
