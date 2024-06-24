9300c8c187363908d2aad3facf4b9187079f6709
Marat Levit <mlevit@gmail.com>
2024-06-24 10:03:18 +1000

    Formatting

--- Lab 01 - SQL/02 Explore.sql
+++ Lab 01 - SQL/02 Explore.sql
@@ -1,4 +1,4 @@
--- Returns all the tables,
+-- Returns all the tables.
 SHOW TABLES IN catalog.database;
 
 
@@ -24,35 +24,19 @@ DESCRIBE EXTENDED catalog.database.fact_apj_sales;
 INSERT INTO
   catalog.database.fact_apj_sales (customer_skey, slocation_skey, sale_id, ts, order_source, order_state, unique_customer_id,store_id)
 VALUES
-  (
-    "3157",
-    "7",
-    "00009e08-3343-4a88-b40d-a66fede2cdff",
-    current_timestamp(),
-    "IN-STORE",
-    "COMPLETED",
-    "SYD01-15",
-    "SYD01"
-  ),
-  (
-    "3523",
-    "5",
-    "00041cc6-30f1-433d-97b5-b92191a92efb",
-    current_timestamp(),
-    "ONLINE",
-    "COMPLETED",
-    "SYD01-48",
-    "SYD01"
-  );
+  ("3157", "7", "00009e08-3343-4a88-b40d-a66fede2cdff", current_timestamp(), "IN-STORE", "COMPLETED", "SYD01-15", "SYD01"),
+  ("3523", "5", "00041cc6-30f1-433d-97b5-b92191a92efb", current_timestamp(), "ONLINE", "COMPLETED", "SYD01-48", "SYD01");
 
 
 -- Return the history information showing the new inserts.
 DESCRIBE HISTORY catalog.database.fact_apj_sales;
 
 
--- Return the number of records in the table.
+-- Return the number of records in version 1 of the table.
 SELECT COUNT(*) FROM catalog.database.fact_apj_sales VERSION AS OF 1;
 
+
+-- Return the number of records in version 2 of the table.
 SELECT COUNT(*) FROM catalog.database.fact_apj_sales VERSION AS OF 2;
 
 
@@ -62,3 +46,13 @@ RESTORE TABLE catalog.database.fact_apj_sales TO VERSION AS OF 1;
 
 -- Return history information showing the restore.
 DESCRIBE HISTORY catalog.database.fact_apj_sales;
+
+
+-- Drop table.
+DROP TABLE catalog.database.store_data_json;
+
+-- Show dropped tables.
+SHOW TABLES DROPPED in catalog.database;
+
+-- Undrop table.
+UNDROP TABLE catalog.database.store_data_json;
