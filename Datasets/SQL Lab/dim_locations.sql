bdddf763a44878d3488b938a71783ae5192aa0a2
Marat Levit <mlevit@gmail.com>
2024-11-19 12:43:48 +1100

    Added new SQL files

---
+++ Datasets/SQL Lab/dim_locations.sql
@@ -0,0 +1,10 @@
+INSERT INTO dim_locations VALUES
+('AKL01', '1', 'Auckland Airport', 'nfuller@example.com', 'Auckland', '976 Reeves Street Webbland 2011', '666 5759', 'NZL'),
+('AKL02', '2', 'Auckland CBD', 'jeremysmith@example.com', 'Auckland', '85 Webb Street MacDonaldmouth 5504', '+64273484326', 'NZL'),
+('BNE02', '3', 'Brisbane Airport', 'castillojoseph@example.net', 'Brisbane', '6 Ware Copse Doughertystad, NSW, 2687', '0425.061.371', 'AUS'),
+('CBR01', '4', 'Canberra Airport', 'feliciasalas@example.com', 'Canberra', '5 Julie Reach Lake Aaron, TAS, 2955', '0495-403-281', 'AUS'),
+('MEL01', '5', 'Melbourne CBD', 'lori00@example.net', 'Melbourne', '3 Tiffany Round Stonehaven, WA, 2687', '(02) 6391 6842', 'AUS'),
+('MEL02', '6', 'Melbourne Airport', 'moralesbrandon@example.com', 'Melbourne', '57 Bryan Circuit New Jason, ACT, 2968', '0777304662', 'AUS'),
+('PER01', '7', 'Perth CBD', 'yhernandez@example.com', 'Perth', 'Level 2 95 Jorge Vale St. Gary, NT, 2705', '08-9854-6006', 'AUS'),
+('SYD01', '8', 'Sydney CBD', 'wardchristopher@example.com', 'Sydney', 'Flat 95 2 Ernest Laneway Alyssaburgh, WA, 2675', '44428748', 'AUS'),
+('WLG01', '9', 'Wellington CBD', 'jonathan47@example.net', 'Wellington', '7 Rowland Reserve RD 2 Arawaka 1086', '(04) 4059380', 'NZL');
