dac0481abaea616ef0f78568c645fdabfba1e9e2
Marat Levit <mlevit@gmail.com>
2024-11-20 12:32:05 +1100

    Cleaned up setup notebook

--- Lab 01 - SQL/01 Setup.py
+++ Lab 01 - SQL/01 Setup.py
@@ -1,4 +1,5 @@
 # Databricks notebook source
+# DBTITLE 1,Create database
 import os
 
 # Get the current user's ID
@@ -17,8 +18,11 @@ spark.sql(f"USE CATALOG {catalog};")
 spark.sql(f"CREATE DATABASE IF NOT EXISTS {database_name};")
 spark.sql(f"USE {database_name}")
 
+print("Database created.")
+
 # COMMAND ----------
 
+# DBTITLE 1,Find and replace
 import os
 
 
@@ -36,97 +40,125 @@ def replace_in_files(directory, old_word, new_word):
                 # Write the file out again
                 with open(file_path, "w", encoding="utf-8") as file:
                     file.write(file_contents)
-                print(f"Updated file: {file_path}")
 
 
 directory_path = f"/Workspace/Users/{current_user_id}/apj-workshops-2024"
 replace_in_files(directory_path, "catalog.database", f"{catalog}.{database_name}")
 replace_in_files(directory_path, "email_address", current_user_id)
 
+print("Workshop files updated with catalog and schema information.")
+
 # COMMAND ----------
 
 # DBTITLE 1,Create dim_customer
-# MAGIC %sql
-# MAGIC CREATE TABLE dim_customer (
-# MAGIC   unique_id VARCHAR(50) NOT NULL,
-# MAGIC   pk INT NOT NULL,
-# MAGIC   id INT NOT NULL,
-# MAGIC   store_id VARCHAR(50) NOT NULL,
-# MAGIC   name VARCHAR(255),
-# MAGIC   email VARCHAR(255),
-# MAGIC   PRIMARY KEY (unique_id)
-# MAGIC );
+spark.sql(
+    """
+      CREATE OR REPLACE TABLE dim_customer (
+        unique_id VARCHAR(50) NOT NULL,
+        pk INT NOT NULL,
+        id INT NOT NULL,
+        store_id VARCHAR(50) NOT NULL,
+        name VARCHAR(255),
+        email VARCHAR(255),
+        PRIMARY KEY (unique_id)
+      )
+    """
+)
+
+print("dim_customer created.")
 
 # COMMAND ----------
 
 # DBTITLE 1,Create dim_locations
-# MAGIC %sql
-# MAGIC CREATE TABLE dim_locations (
-# MAGIC   id VARCHAR(50) NOT NULL,
-# MAGIC   pk INT NOT NULL,
-# MAGIC   name VARCHAR(255),
-# MAGIC   email VARCHAR(255),
-# MAGIC   city VARCHAR(100),
-# MAGIC   hq VARCHAR(255),
-# MAGIC   phone_number VARCHAR(50),
-# MAGIC   country_code VARCHAR(10),
-# MAGIC   PRIMARY KEY (id)
-# MAGIC );
+spark.sql(
+    """
+      CREATE TABLE dim_locations (
+        id VARCHAR(50) NOT NULL,
+        pk INT NOT NULL,
+        name VARCHAR(255),
+        email VARCHAR(255),
+        city VARCHAR(100),
+        hq VARCHAR(255),
+        phone_number VARCHAR(50),
+        country_code VARCHAR(10),
+        PRIMARY KEY (id)
+      )
+    """
+)
+
+print("dim_locations created.")
 
 # COMMAND ----------
 
 # DBTITLE 1,Create dim_products
-# MAGIC %sql
-# MAGIC CREATE TABLE dim_products (
-# MAGIC   product_skey INT NOT NULL,
-# MAGIC   id VARCHAR(255) NOT NULL,
-# MAGIC   ingredients STRING,
-# MAGIC   name VARCHAR(255),
-# MAGIC   current_record CHAR(1),
-# MAGIC   start_date TIMESTAMP,
-# MAGIC   end_date TIMESTAMP,
-# MAGIC   PRIMARY KEY (id)
-# MAGIC );
+spark.sql(
+    """
+      CREATE TABLE dim_products (
+        product_skey INT NOT NULL,
+        id VARCHAR(255) NOT NULL,
+        ingredients STRING,
+        name VARCHAR(255),
+        current_record CHAR(1),
+        start_date TIMESTAMP,
+        end_date TIMESTAMP,
+        PRIMARY KEY (id)
+      )
+    """
+)
+
+print("dim_products created.")
 
 # COMMAND ----------
 
 # DBTITLE 1,Create fact_apj_sale_items
-# MAGIC %sql
-# MAGIC CREATE TABLE fact_apj_sale_items (
-# MAGIC     sale_id VARCHAR(255) NOT NULL,
-# MAGIC     product_id VARCHAR(255),
-# MAGIC     store_id VARCHAR(50),
-# MAGIC     product_size VARCHAR(50),
-# MAGIC     product_cost DECIMAL(10, 2),
-# MAGIC     product_ingredients STRING,
-# MAGIC     product_skey INT,
-# MAGIC     slocation_skey INT,
-# MAGIC     unique_customer_id VARCHAR(255),
-# MAGIC     PRIMARY KEY (sale_id),
-# MAGIC     FOREIGN KEY (unique_customer_id) REFERENCES dim_customer(unique_id),
-# MAGIC     FOREIGN KEY (store_id) REFERENCES dim_locations(id)
-# MAGIC );
+spark.sql(
+    """
+      CREATE TABLE fact_apj_sale_items (
+        sale_id VARCHAR(255) NOT NULL,
+        product_id VARCHAR(255),
+        store_id VARCHAR(50),
+        product_size VARCHAR(50),
+        product_cost DECIMAL(10, 2),
+        product_ingredients STRING,
+        product_skey INT,
+        slocation_skey INT,
+        unique_customer_id VARCHAR(255),
+        PRIMARY KEY (sale_id),
+        FOREIGN KEY (unique_customer_id) REFERENCES dim_customer(unique_id),
+        FOREIGN KEY (store_id) REFERENCES dim_locations(id)
+      )
+    """
+)
+
+print("fact_apj_sale_items created.")
 
 # COMMAND ----------
 
-# MAGIC %sql
-# MAGIC CREATE TABLE fact_apj_sales (
-# MAGIC     sale_id STRING,
-# MAGIC     ts TIMESTAMP,
-# MAGIC     order_source STRING,
-# MAGIC     order_state STRING,
-# MAGIC     unique_customer_id STRING,
-# MAGIC     store_id STRING,
-# MAGIC     customer_skey INT,
-# MAGIC     slocation_skey INT,
-# MAGIC     PRIMARY KEY (sale_id),
-# MAGIC     FOREIGN KEY (unique_customer_id) REFERENCES dim_customer(unique_id)
-# MAGIC )
+# DBTITLE 1,Create fact_apj_sales
+spark.sql(
+    """
+      CREATE TABLE fact_apj_sales (
+        sale_id STRING,
+        ts TIMESTAMP,
+        order_source STRING,
+        order_state STRING,
+        unique_customer_id STRING,
+        store_id STRING,
+        customer_skey INT,
+        slocation_skey INT,
+        PRIMARY KEY (sale_id),
+        FOREIGN KEY (unique_customer_id) REFERENCES dim_customer(unique_id)
+      )
+    """
+)
+
+print("fact_apj_sales created.")
 
 # COMMAND ----------
-
 
 # DBTITLE 1,Define read file function
+
+
 def read_file_contents(file_path):
     with open(file_path, "r", encoding="utf-8") as file:
         return file.read()
@@ -137,75 +169,46 @@ def read_file_contents(file_path):
 # DBTITLE 1,Insert dim_customer
 spark.sql(read_file_contents("../Datasets/SQL Lab/dim_customer.sql"))
 
+print("dim_customer records inserted.")
+
 # COMMAND ----------
 
 # DBTITLE 1,Insert dim_locations
 spark.sql(read_file_contents("../Datasets/SQL Lab/dim_locations.sql"))
+
+print("dim_locations records inserted.")
 
 # COMMAND ----------
 
 # DBTITLE 1,Insert dim_products
 spark.sql(read_file_contents("../Datasets/SQL Lab/dim_products.sql"))
 
+print("dim_products records inserted.")
+
 # COMMAND ----------
 
 # DBTITLE 1,Insert fact_apj_sales
 spark.sql(read_file_contents("../Datasets/SQL Lab/fact_apj_sales.sql"))
+
+print("fact_apj_sales records inserted.")
 
 # COMMAND ----------
 
 # DBTITLE 1,Insert fact_apj_sale_items
 spark.sql(read_file_contents("../Datasets/SQL Lab/fact_apj_sale_items.sql"))
 
+print("fact_apj_sale_items records inserted.")
+
 # COMMAND ----------
 
-# MAGIC %sql
-# MAGIC CREATE OR REPLACE TABLE store_data_json AS
-# MAGIC SELECT
-# MAGIC   1 AS id,
-# MAGIC   '{
-# MAGIC    "store":{
-# MAGIC       "fruit": [
-# MAGIC         {"weight":8,"type":"apple"},
-# MAGIC         {"weight":9,"type":"pear"}
-# MAGIC       ],
-# MAGIC       "basket":[
-# MAGIC         [1,2,{"b":"y","a":"x"}],
-# MAGIC         [3,4],
-# MAGIC         [5,6]
-# MAGIC       ],
-# MAGIC       "book":[
-# MAGIC         {
-# MAGIC           "author":"Nigel Rees",
-# MAGIC           "title":"Sayings of the Century",
-# MAGIC           "category":"reference",
-# MAGIC           "price":8.95
-# MAGIC         },
-# MAGIC         {
-# MAGIC           "author":"Herman Melville",
-# MAGIC           "title":"Moby Dick",
-# MAGIC           "category":"fiction",
-# MAGIC           "price":8.99,
-# MAGIC           "isbn":"0-553-21311-3"
-# MAGIC         },
-# MAGIC         {
-# MAGIC           "author":"J. R. R. Tolkien",
-# MAGIC           "title":"The Lord of the Rings",
-# MAGIC           "category":"fiction",
-# MAGIC           "reader":[
-# MAGIC             {"age":25,"name":"bob"},
-# MAGIC             {"age":26,"name":"jack"}
-# MAGIC           ],
-# MAGIC           "price":22.99,
-# MAGIC           "isbn":"0-395-19395-8"
-# MAGIC         }
-# MAGIC       ],
-# MAGIC       "bicycle":{
-# MAGIC         "price":19.95,
-# MAGIC         "color":"red"
-# MAGIC       }
-# MAGIC     },
-# MAGIC     "owner":"amy",
-# MAGIC     "zip code":"94025",
-# MAGIC     "fb:testid":"1234"
-# MAGIC  }' as raw;
+# DBTITLE 1,Create store_data_json
+spark.sql(
+    """
+      CREATE OR REPLACE TABLE store_data_json AS
+        SELECT
+          1 AS id,
+          '{"store":{"fruit":[{"weight":8,"type":"apple"},{"weight":9,"type":"pear"}],"basket":[[1,2,{"b":"y","a":"x"}],[3,4],[5,6]],"book":[{"author":"Nigel Rees","title":"Sayings of the Century","category":"reference","price":8.95},{"author":"Herman Melville","title":"Moby Dick","category":"fiction","price":8.99,"isbn":"0-553-21311-3"},{"author":"J. R. R. Tolkien","title":"The Lord of the Rings","category":"fiction","reader":[{"age":25,"name":"bob"},{"age":26,"name":"jack"}],"price":22.99,"isbn":"0-395-19395-8"}],"bicycle":{"price":19.95,"color":"red"}},"owner":"amy","zip code":"94025","fb:testid":"1234"}' as raw
+    """
+)
+
+print("store_data_json created.")
