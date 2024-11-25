ac0a0b3b4e20ba210ff6697d3d4d84c891332424
Marat Levit <mlevit@gmail.com>
2024-11-25 13:48:20 +1100

    Converted JSON to variant

--- Lab 01 - SQL/01 Setup.py
+++ Lab 01 - SQL/01 Setup.py
@@ -207,7 +207,7 @@ spark.sql(
       CREATE OR REPLACE TABLE store_data_json AS
         SELECT
           1 AS id,
-          '{"store":{"fruit":[{"weight":8,"type":"apple"},{"weight":9,"type":"pear"}],"basket":[[1,2,{"b":"y","a":"x"}],[3,4],[5,6]],"book":[{"author":"Nigel Rees","title":"Sayings of the Century","category":"reference","price":8.95},{"author":"Herman Melville","title":"Moby Dick","category":"fiction","price":8.99,"isbn":"0-553-21311-3"},{"author":"J. R. R. Tolkien","title":"The Lord of the Rings","category":"fiction","reader":[{"age":25,"name":"bob"},{"age":26,"name":"jack"}],"price":22.99,"isbn":"0-395-19395-8"}],"bicycle":{"price":19.95,"color":"red"}},"owner":"amy","zip code":"94025","fb:testid":"1234"}' as raw
+          parse_json('{"store":{"fruit":[{"weight":8,"type":"apple"},{"weight":9,"type":"pear"}],"basket":[[1,2,{"b":"y","a":"x"}],[3,4],[5,6]],"book":[{"author":"Nigel Rees","title":"Sayings of the Century","category":"reference","price":8.95},{"author":"Herman Melville","title":"Moby Dick","category":"fiction","price":8.99,"isbn":"0-553-21311-3"},{"author":"J. R. R. Tolkien","title":"The Lord of the Rings","category":"fiction","reader":[{"age":25,"name":"bob"},{"age":26,"name":"jack"}],"price":22.99,"isbn":"0-395-19395-8"}],"bicycle":{"price":19.95,"color":"red"}},"owner":"amy","zip code":"94025","fb:testid":"1234"}') as raw
     """
 )
 
